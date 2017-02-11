39429f300c2c8035767d2b512ae4737c8489958a
Stefano Cascavilla <cascavilla1@gmail.com>
2017-02-11 11:33:05 +0100

    Add source code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var engines = require('consolidate');
+var fs = require('fs'); 
 
 app.use(express.static('./views'));
 app.use(express.static('./media'));
@@ -11,8 +12,19 @@ app.set('view engine', 'hbs');
 
 var texts;
 
+fs.readFile('texts.json', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  texts = JSON.parse(data);
+  console.log('File letto con successo')
+});
+
 app.get('/', function(req, res){
-  res.render('home.hbs');
+  res.render('home.hbs', {
+    texts: texts
+  });
 });
 
 app.listen(3000, function(){
